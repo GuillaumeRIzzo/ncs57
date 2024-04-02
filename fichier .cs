ae396d0d31920e91e2b49f8f0648b1a3944c07b3
Guillaume ncs57 <guillaume.rizzo.lomet@gmail.com>
2024-04-02 13:57:23 +0200

    file to rebase

--- fichier .cs
+++ fichier .cs
@@ -5,7 +5,11 @@ class Program
     static void Main()
     {
         Console.WriteLine("not french");
+<<<<<<< HEAD
 	    Console.WriteLine("merge conflict");
         Console.WriteLine("je suis le conflit et gère moi avec git rebase ");
+=======
+	Console.WriteLine("merge conflict");
+>>>>>>> 1aca7fd (file to rebase)
     }
 }
