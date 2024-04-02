e031eac655f236e17e091c58934cc684aaef379d
Guillaume ncs57 <guillaume.rizzo.lomet@gmail.com>
2024-04-02 14:24:56 +0200

    Add Imc project

---
+++ imc/Imc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vde
+{
+    class Imc
+    {
+        public double Taille { get; set; }
+        public double Poid { get; set; }
+
+        public Imc(double taille, double poid)
+        {
+            Taille = taille;
+            Poid = poid;
+        }
+
+        public double Calc()
+        {
+            double imc = Poid / (Taille * Taille) * 10000;
+            return imc;
+        }
+
+        public string Result()
+        {
+            string result;
+
+            switch (this.Calc())
+            {
+                case > 40:
+                    result = "Obésité morbide ou massive";
+                    break;
+                case > 35:
+                    result = "Obésité sévère";
+                    break;
+                case > 30:
+                    result = "Obésité modérée";
+                    break;
+                case > 25:
+                    result = "Surpoid";
+                    break;
+                case > 18.5:
+                    result = "Corpulence normale";
+                    break;
+                default:
+                    result = "Insuffisance pondérale (maigreur)";
+                    break;
+            }
+            return result;
+        }
+    }
+}
---
+++ imc/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Vde
+{
+   class Program
+   {
+        static void Main(string[] args)
+        {
+            double taille;
+            double poid;
+
+            /*Console.WriteLine("Entrez votre taille");
+            string? enter = Console.ReadLine();*/
+
+            try
+            {
+                Console.WriteLine("Entrez votre taille");
+                taille = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Entrez votre poid");
+                poid = Convert.ToDouble(Console.ReadLine());
+
+                Vde.Imc imc = new Imc(taille, poid);
+
+                Console.WriteLine(imc.Result());
+                Console.ReadLine();
+            }
+            catch (SystemException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            /*while (!double.TryParse(enter, out _)) 
+            {
+                Console.WriteLine("Entrez votre taille en chiffre numérique");
+                enter = Console.ReadLine();
+            }
+            taille = Convert.ToDouble(enter);
+
+            Console.WriteLine("Entrez votre poid");
+            enter = Console.ReadLine();
+
+            while (!double.TryParse(enter, out _)) 
+            {
+                Console.WriteLine("Entrez votre poid en chiffre numérique");
+                enter = Console.ReadLine();
+            
+            }
+                poid = Convert.ToDouble(enter);
+                Vde.Imc imc = new Imc(taille, poid);
+
+                Console.WriteLine(imc.Result());
+                Console.ReadLine();
+            */
+
+        }
+    }
+}
