2226b03a48ccb7ac5ee8de69f5f4115b7c066df6
Guillaume ncs57 <guillaume.rizzo.lomet@gmail.com>
2024-04-10 08:45:12 +0200

    Add StudentAPI Project

---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentAPI.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(250)")]
+        public string Name { get; set; } = string.Empty;
+        public string ContactNumber { get; set; } = string.Empty;
+        public int Age { get; set; }
+    }
+}
